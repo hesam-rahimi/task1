df9bc82394bc0635d7422c117806c28088f11aa9
hesam <hesam82rhm@gmail.com>
2024-04-29 15:57:51 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,217 +1,64 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import TodoBox from "./components/TodoBox/TodoBox";
-import SelectedBox from "./components/SelectedBox/SelectedBox";
-
-const datas = [
-  {
-    id: "A",
-    title: "item 1",
-    created: "2022-12-03T07:22:25.911Z",
-    uniqueId: 1,
-  },
-  {
-    id: "A",
-    title: "item 2",
-    created: "2022-08-15T07:22:25.911Z",
-    uniqueId: 2,
-  },
-  {
-    id: "A",
-    title: "item 3",
-    created: "2021-07-28T07:22:25.911Z",
-    uniqueId: 3,
-  },
-  {
-    id: "A",
-    title: "item 4",
-    created: "2022-05-15T07:22:25.911Z",
-    uniqueId: 4,
-  },
-  {
-    id: "A",
-    title: "item 5",
-    created: "2021-11-07T07:22:25.911Z",
-    uniqueId: 5,
-  },
-  {
-    id: "B",
-    title: "item 6",
-    created: "2022-04-10T07:22:25.911Z",
-    uniqueId: 6,
-  },
-  {
-    id: "B",
-    title: "item 7",
-    created: "2022-12-03T07:22:25.911Z",
-    uniqueId: 7,
-  },
-  {
-    id: "B",
-    title: "item 8",
-    created: "2022-08-15T07:22:25.911Z",
-    uniqueId: 8,
-  },
-  {
-    id: "C",
-    title: "item 9",
-    created: "2021-07-28T07:22:25.911Z",
-    uniqueId: 9,
-  },
-  {
-    id: "C",
-    title: "item 10",
-    created: "2022-05-15T07:22:25.911Z",
-    uniqueId: 10,
-  },
-  {
-    id: "C",
-    title: "item 11",
-    created: "2021-11-07T07:22:25.911Z",
-    uniqueId: 11,
-  },
-  {
-    id: "C",
-    title: "item 12",
-    created: "2022-04-10T07:22:25.911Z",
-    uniqueId: 12,
-  },
-  {
-    id: "C",
-    title: "item 13",
-    created: "2022-12-03T07:22:25.911Z",
-    uniqueId: 13,
-  },
-  {
-    id: "D",
-    title: "item 14",
-    created: "2022-08-15T07:22:25.911Z",
-    uniqueId: 14,
-  },
-  {
-    id: "D",
-    title: "item 15",
-    created: "2021-07-28T07:22:25.911Z",
-    uniqueId: 15,
-  },
-  {
-    id: "D",
-    title: "item 16",
-    created: "2022-05-15T07:22:25.911Z",
-    uniqueId: 16,
-  },
-  {
-    id: "D",
-    title: "item 17",
-    created: "2021-11-07T07:22:25.911Z",
-    uniqueId: 17,
-  },
-  {
-    id: "E",
-    title: "item 18",
-    created: "2022-04-10T07:22:25.911Z",
-    uniqueId: 18,
-  },
-  {
-    id: "E",
-    title: "item 19",
-    created: "2021-11-07T07:22:25.911Z",
-    uniqueId: 19,
-  },
-  {
-    id: "E",
-    title: "item 20",
-    created: "2022-04-10T07:22:25.911Z",
-    uniqueId: 20,
-  },
-];
+import SelectedBox from "./components/SelectedBox";
+import { DATA, grouping, unGrouping } from "./helper/indes";
 
 function App() {
-  const [rightData, setRightData] = useState([]);
-  const [leftData, setLeftData] = useState([]);
-
   const [checked, setChecked] = useState({});
+  const [rightData, setRightData] = useState([]);
+  const [leftData, setLeftData] = useState(grouping(DATA));
 
-  const [sortedDatas, setSortedDatas] = useState({});
-
-  useEffect(() => {
-    const sortedTodos = datas.reduce((prev, crr) => {
-      !prev[crr.id]
-        ? (prev[crr.id] = [{ ...crr }])
-        : prev[crr.id].push({ ...crr });
-      return prev;
-    }, {});
-
-    setSortedDatas(sortedTodos);
-  }, []);
-
-  const checkAll = (id) => {
-    const updatedData = { ...sortedDatas };
-    updatedData[id] = updatedData[id].map((item) => ({
-      ...item,
-      isSelect: true,
-    }));
-    setSortedDatas(updatedData);
+  const addHandler = () => {
+    setLeftData((leftPrev) => {
+      const notGroupedData = unGrouping(leftPrev);
+      const newRightData = notGroupedData.filter((item) => item.uniqueId in checked);
+      const newLeftData = notGroupedData.filter((item) => !(item.uniqueId in checked));
+      setRightData((rightPrev) => {
+        return grouping([...newRightData, ...unGrouping(rightPrev)]);
+      });
+      return grouping(newLeftData);
+    });
+    setChecked({});
   };
 
-  const changeData = () => {
-    setSortedDatas((prev) => ({ ...prev }));
+  const deleteHandler = () => {
+    setRightData((rightPrev) => {
+      const notGroupedData = unGrouping(rightPrev);
+      const newRightData = notGroupedData.filter((item) => !(item.uniqueId in checked));
+      const newLeftData = notGroupedData.filter((item) => item.uniqueId in checked);
+      setLeftData((leftPrev) => {
+        return grouping([...newLeftData, ...unGrouping(leftPrev)]);
+      });
+      return grouping(newRightData);
+    });
+    setChecked({});
   };
 
   return (
     <div className="bg-gray-600 min-h-screen w-full py-8 px-8 flex">
+      {/* left */}
       <div className="w-1/2 bg-white rounded-xl py-6 px-3 mr-3">
         <p className="text-center font-bold border-b pb-2 text-lg">All Data</p>
-        <button
-          className="w-full bg-gray-500 mt-2 py-2 text-white rounded-md"
-          onClick={changeData}
-        >
+        <button className="w-full bg-gray-500 mt-2 py-2 text-white rounded-md" onClick={addHandler}>
           Add
         </button>
         <div>
-          {Object.keys(sortedDatas).map(
-            (id) =>
-              sortedDatas[id].filter((data) => data.isSelect === false).length >
-                0 && (
-                <TodoBox
-                  key={id}
-                  id={id}
-                  setChecked={setChecked}
-                  checked={checked}
-
-
-
-                />
-              )
-          )}
+          {leftData.map((item) => {
+            return <SelectedBox key={item[0].uniqueId} item={item} checked={checked} setChecked={setChecked} />;
+          })}
         </div>
       </div>
 
       {/* right */}
-
       <div className="w-1/2 bg-white rounded-xl py-6 px-3 ml-3">
-        <p className="text-center font-bold border-b pb-2 text-lg">
-          Selected Data
-        </p>
-        <button
-          className="w-full bg-gray-500 mt-2 py-2 text-white rounded-md"
-          onClick={changeData}
-        >
+        <p className="text-center font-bold border-b pb-2 text-lg">Selected Data</p>
+        <button className="w-full bg-gray-500 mt-2 py-2 text-white rounded-md" onClick={deleteHandler}>
           Delete
         </button>
         <div>
-          {Object.keys(sortedDatas).map(
-            (id) =>
-              sortedDatas[id].filter((data) => data.isSelect === true).length >
-                0 && (
-                <SelectedBox
-                  key={id}
-                  id={id}
-                  setChecked={setChecked}
-                  checked={checked}
-                />
-              )
-          )}
+          {rightData.map((item) => {
+            return <SelectedBox key={item[0].uniqueId} item={item} checked={checked} setChecked={setChecked} />;
+          })}
         </div>
       </div>
     </div>
---
+++ src/components/SelectedBox.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+
+const SelectedBox = ({ item, checked, setChecked }) => {
+  const [isShowDetail, setIsShowDetail] = useState(false);
+
+  const onChange = (e) => {
+    const valueKey = e.target.value;
+    const checked = e.target.checked;
+    if (checked) {
+      setChecked((prev) => ({ ...prev, [valueKey]: true }));
+    } else
+      setChecked((prev) => {
+        const newData = { ...prev };
+        delete newData[valueKey];
+        return newData;
+      });
+  };
+
+  const onChangeAll = (e) => {
+    const checked = e.target.checked;
+
+    if (checked) {
+      const ids = item.reduce((prev, current) => {
+        prev = { ...prev, [current.uniqueId]: true };
+        return prev;
+      }, {});
+      setChecked((prev) => ({ ...prev, ...ids }));
+    } else {
+      setChecked((prev) => {
+        const newData = { ...prev };
+        item.forEach(({ uniqueId }) => delete newData[uniqueId]);
+        return newData;
+      });
+    }
+  };
+
+  return (
+    <div>
+      <div className="bg-gray-200 my-2 py-3 px-2 flex justify-between items-center rounded-md">
+        <span className="text-white bg-sky-800 w-9 h-9 rounded-md flex justify-center items-center">{item[0].id}</span>
+        <div>quantity :{item.length}</div>
+        <div className="flex gap-2">
+          <input type="checkbox" className="cursor-pointer" onChange={onChangeAll} checked={item.every(({ uniqueId }) => uniqueId in checked)} />
+          <IoIosArrowDown
+            fontSize={20}
+            cursor="pointer"
+            onClick={() => setIsShowDetail((prev) => !prev)}
+            className={isShowDetail ? "rotate-180 transition-transform duration-300" : "rotate-0 transition-transform duration-300"}
+          />
+        </div>
+      </div>
+      {isShowDetail &&
+        item.map((todo) => (
+          <div key={todo.uniqueId} className="flex justify-between items-center bg-slate-300 my-2 py-1 rounded-lg px-3">
+            <span>#{todo.uniqueId}</span>
+            <span>{todo.created.slice(0, 10)}</span>
+            <input type="checkbox" className="cursor-pointer" checked={todo.uniqueId in checked} value={todo.uniqueId} onChange={onChange} />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default SelectedBox;
